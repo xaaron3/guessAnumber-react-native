b576b8aa5fd8869c5c3ff53b3a5aef9050ca0ad3
aaron <xakong@outlook.com>
2020-03-05 00:15:14 -0500

    ending commit

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import TitleText from './TitleText';
+import Colors from '../constants/colors';
+
 const Header = props => {
   return (
     <View style={styles.header}>
-      <Text style={styles.headerTitle}>{props.title}</Text>
+      <TitleText>{props.title}</TitleText>
     </View>
   );
 };
@@ -14,13 +17,9 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: '#f7287b',
+    backgroundColor: Colors.primary,
     alignItems: 'center',
     justifyContent: 'center'
-  },
-  headerTitle: {
-    color: 'black',
-    fontSize: 18
   }
 });
 
